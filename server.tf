96e40ab9315caddd05f5d463e1eaca59d199c67b
chashokmca <chashokmca@gmail.com>
2023-05-26 21:28:22 +0530

    Intial commit

--- server.tf
+++ server.tf
@@ -1,5 +1,11 @@
+data "aws_ami" "centos" {
+  owners = ["973714476881"]
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+}
+
 resource "aws_instance" "frontend" {
-  ami           = "ami-0b5a2b5b8f2be4ec2" # us-west-2
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t2.micro"
 
   tags = {
