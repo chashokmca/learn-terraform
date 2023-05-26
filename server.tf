4f7c343733e5ddb2e6b51bcd037d0abfa1108ccc
chashokmca <chashokmca@gmail.com>
2023-05-26 22:22:42 +0530

    Intial commit

--- server.tf
+++ server.tf
@@ -9,7 +9,7 @@ data "aws_ami" "centos" {
 }
 
 resource "aws_instance" "frontend" {
-  count   = length(vars.components)
+  count   = length(var.components)
   ami           = data.aws_ami.centos.image_id
   instance_type = "t2.micro"
 
