0aaae92db9882a358e70ef80c930b3ccbda3b025
chashokmca <chashokmca@gmail.com>
2023-05-26 21:51:08 +0530

    Intial commit

--- server.tf
+++ server.tf
@@ -13,6 +13,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  name    = "frontend-dev.adevops72.online"
+  type    = "A"
+  zone_id = "Z064328134L0JD7T8XRZ9"
+  ttl = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 output "public_ip" {
   value = aws_instance.frontend.public_ip
 }
