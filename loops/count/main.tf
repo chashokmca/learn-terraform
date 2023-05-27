31e2384525fd485842ca5f9ce80f6f9a127ae8d9
chashokmca <chashokmca@gmail.com>
2023-05-27 16:20:00 +0530

    Intial commit

--- loops/count/main.tf
+++ loops/count/main.tf
@@ -34,3 +34,11 @@ resource "null_resource" "fruits1" {
   }
 }
 
+resource "null_resource" "fruits2" {
+  count = length(var.fruits1)
+
+  provisioner "local-exec" {
+    command = "echo ${var.fruits1[count.index]}"
+  }
+}
+
