42ef804d7d0460802cb77128deaa3abde344eb49
chashokmca <chashokmca@gmail.com>
2023-05-27 16:55:43 +0530

    Intial commit

--- loops/count/main.tf
+++ loops/count/main.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "null" {
+/*resource "null_resource" "null" {
   count = 10
 }
 
@@ -32,7 +32,7 @@ resource "null_resource" "fruits1" {
   provisioner "local-exec" {
     command = "echo ${each.key} ----  ${each.value}"
   }
-}
+}*/
 
 variable "fruits2" {
   default = {
