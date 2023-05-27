7b0260055f93920554af36a3ad7f96feaef08a93
chashokmca <chashokmca@gmail.com>
2023-05-27 13:37:29 +0530

    Intial commit

--- datasource/main.tf
+++ datasource/main.tf
@@ -17,6 +17,11 @@ output "security_group_sel" {
 
 data "aws_security_groups" "groups" {}
 
+data "aws_security_group" "group" {
+  count = length(data.aws_security_groups.groups.ids)
+  id = data.aws_security_groups.groups.id[count.index]
+}
+
 output "aws_security_groups" {
-  value = data.aws_security_groups.groups
+  value = data.aws_security_group.group.description
 }
