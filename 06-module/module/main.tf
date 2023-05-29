05889403f35b373ff6b5e400cc42220047e30c6e
chashokmca <chashokmca@gmail.com>
2023-05-29 22:26:28 +0530

    Intial commit

--- 06-module/module/main.tf
+++ 06-module/module/main.tf
@@ -1,6 +1,7 @@
 resource "null_resource" "nothing" {
   provisioner "local-exec" {
     command = "echo Hello from Module - Input - ${var.input}"
+    command = "echo Hello from Module - name - ${var.name}"
   }
 }
 
